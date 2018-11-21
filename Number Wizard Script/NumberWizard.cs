41682a5f709a14e1556812635ac9370e98ac2b2f
Anna Ryder <annaelizabethryder@gmail.com>
2018-11-21 22:03:57 +0000

    Update NumberWizard.cs

--- Number Wizard Script/NumberWizard.cs
+++ Number Wizard Script/NumberWizard.cs
@@ -1,1 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a number");
+        Debug.Log("The highest number is : " + max);
+        Debug.Log("The lowest number is : " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Press Up = Higher , Press Down = Lower , Press Enter = Correct");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
 
